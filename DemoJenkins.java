1a2052dcc2811efa5215245ab83d057750c837fb
tpfkcuf <heolovegau1811@gmail.com>
2017-07-07 10:41:47 +0700

    Update DemoJenkins.java

--- DemoJenkins.java
+++ DemoJenkins.java
@@ -2,7 +2,7 @@
 public class DemoJenkins {
 
     public static void main(String[] args) {
-        for(int i = 0; i <= 5; i++) {
+        for(int i = 0; i <= 10; i++) {
             System.out.println("Hello World");
         }                
     }
